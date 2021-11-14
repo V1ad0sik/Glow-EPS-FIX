da83ee97f3c26a4f9579a66070e1dbc45f9d4dcc
SQL <i.sql@inbox.ru>
2021-11-14 13:13:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -23,27 +23,25 @@ def ESP():
     while True:
         player = pm.read_int(client + dwLocalPlayer)
         glow_manager = pm.read_int(client + dwGlowObjectManager)
-        team  = pm.read_int(player + m_iTeamNum)
 
-        for i in range(1, 32):
-            entity = pm.read_int(client + dwEntityList + i * 0x10)
-
-            if entity:
-                entity_team_id = pm.read_int(entity + m_iTeamNum)
-                entity_glow = pm.read_int(entity + m_iGlowIndex)
-
-                if entity_team_id != team :
-                    pm.write_float(glow_manager + entity_glow * 0x38 + 0x8, float(1))
-                    pm.write_float(glow_manager + entity_glow * 0x38 + 0xC, float(1))
-                    pm.write_float(glow_manager + entity_glow * 0x38 + 0x10, float(1))
-                    pm.write_float(glow_manager + entity_glow * 0x38 + 0x14, float(1))
-
-                    pm.write_int(glow_manager + entity_glow * 0x38 + 0x28, 1)
-
-                    time.sleep(0.01)
-
-
+        if (player):
+            team  = pm.read_int(player + m_iTeamNum)
+            
+            for i in range(1, 32):
+                entity = pm.read_int(client + dwEntityList + i * 0x10)
+                
+                if (entity):
+                    entity_team_id = pm.read_int(entity + m_iTeamNum)
+                    entity_glow = pm.read_int(entity + m_iGlowIndex)
+                    
+                    if (entity_team_id != team):
+                        pm.write_float(glow_manager + entity_glow * 0x38 + 0x8, float(1))
+                        pm.write_float(glow_manager + entity_glow * 0x38 + 0xC, float(1))
+                        pm.write_float(glow_manager + entity_glow * 0x38 + 0x10, float(1))
+                        pm.write_float(glow_manager + entity_glow * 0x38 + 0x14, float(1))
+                        
+                        pm.write_int(glow_manager + entity_glow * 0x38 + 0x28, 1)
+                        
+        time.sleep(0.01)                 
+          
 ESP()
-
-
-
